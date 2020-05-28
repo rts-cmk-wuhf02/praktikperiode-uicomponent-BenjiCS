dc7d802ddfa23464df0aec85ca1d4b628c192d5b
Benjamin Hansen <4463738@RTS-365.dk>
2020-05-28 09:47:15 +0200

    maldives component "Done"

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 
 // Components
 import Maladives from './assets/components/maldives/app';
-import Outdoor from './assets/components/outdoor/app';
-import Parcelbox from './assets/components/parcelbox/app';
-import Offthegrid from './assets/components/offthegrid/app'; // eslint-disable-line no-unused-vars
-import Mustsee from './assets/components/mustsee/app'; // eslint-disable-line no-unused-vars
-import Latestnews from './assets/components/latestnews/app'; // eslint-disable-line no-unused-vars
-import Food from './assets/components/food/app'; // eslint-disable-line no-unused-vars
-import Tecture from './assets/components/tecture/app'; // eslint-disable-line no-unused-vars
-import Visual from './assets/components/visual/app'; // eslint-disable-line no-unused-vars
-import Readmore from './assets/components/readmore/app'; // eslint-disable-line no-unused-vars
+// import Outdoor from './assets/components/outdoor/app';
+// import Parcelbox from './assets/components/parcelbox/app';
+// import Offthegrid from './assets/components/offthegrid/app'; // eslint-disable-line no-unused-vars
+// import Mustsee from './assets/components/mustsee/app'; // eslint-disable-line no-unused-vars
+// import Latestnews from './assets/components/latestnews/app'; // eslint-disable-line no-unused-vars
+// import Food from './assets/components/food/app'; // eslint-disable-line no-unused-vars
+// import Tecture from './assets/components/tecture/app'; // eslint-disable-line no-unused-vars
+// import Visual from './assets/components/visual/app'; // eslint-disable-line no-unused-vars
+// import Readmore from './assets/components/readmore/app'; // eslint-disable-line no-unused-vars
 
 
 import * as serviceWorker from './serviceWorker';
@@ -22,8 +22,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Maladives/>
-    <Outdoor/>
-   <Parcelbox/>
+    {/* <Outdoor/>
+   <Parcelbox/> */}
     { /* <Offthegrid/>
     <Mustsee/>
     <Latestnews/>
